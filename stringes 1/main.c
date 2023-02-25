987db6b8e24c0331335cbf823d551adcf7ad2057
Enmo7 <114494116+Enmo7@users.noreply.github.com>
2023-02-25 09:37:17 -0800

    Add files via upload

---
+++ stringes 1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char arr[7];
+
+
+
+    printf("Please inter your name : \n");
+
+    scanf("%s", &arr);
+
+    printf("%s ", arr);
+
+    return 0;
+}
